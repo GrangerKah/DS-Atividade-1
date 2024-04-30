1998240b8d561072943f53ae9821df4ef2705271
Ana Carolina <166462848+GrangerKah@users.noreply.github.com>
2024-04-29 22:19:25 -0300

    Add files via upload

---
+++ CRUDAPI.sql
@@ -0,0 +1,18 @@
+create database crudapi;
+
+USE crudapi;
+
+CREATE TABLE USUARIO(
+	ID INT NOT null auto_increment primary KEY,
+    NOME VARCHAR(50),
+    IDADE INT
+);
+
+insert into USUARIO (NOME, IDADE) values ('Ana', 29);
+insert into USUARIO (NOME, IDADE) values ('Rafa', 28);
+insert into USUARIO (NOME, IDADE) values ('Matheus', 24);
+insert into USUARIO (NOME, IDADE) values ('Ana', 29);
+
+DELETE FROM usuario WHERE ID = 1 ;
+
+select *from usuario;
